a487be03c32daa47d1df055c08ba95ef15cae8d3
E-Jhay Esplana <esplanaejhaym@gmail.com>
2025-01-21 18:21:46 +0800

    Authorized only one user for a specific command

--- cogs/math.py
+++ cogs/math.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 import discord
 from discord.ext import commands
 
+load_dotenv()
+user_id = os.getenv('USER_ID')
 class Math(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -20,8 +25,11 @@ class Math(commands.Cog):
 
     @commands.command()
     async def troll(self, ctx, count: int):
+        if str(ctx.author.id) != user_id:
+            await ctx.reply(f'{ctx.author.mention}, You are not allowed to use this command')
+            return
         for i in range(count):
-            await ctx.send(f'{i + 1}. Spamming messages...')
+            await ctx.send(f'Spamming messages...{i}x')
 
 def setup(bot):
     bot.add_cog(Math(bot)) 
