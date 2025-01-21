506c5c0a75a925074e1e8e0682c1f1cdf710115f
E-Jhay Esplana <esplanaejhaym@gmail.com>
2025-01-21 18:26:54 +0800

    Created new cog

--- main.py
+++ main.py
@@ -24,22 +24,7 @@ intents.message_content = True
 
 bot = Main(intents=intents)
 
-@bot.command()
-async def hello( ctx):
-        await ctx.reply(f'Hello {ctx.author.mention}')
 
-@bot.command()
-async def show_commands( ctx):
-    command_list = (
-        "📋 **Available Commands**:\n\n"
-        "➕ **!add <a> <b>**: Add two numbers together\n"
-        "📊 **!get_mean <numbers...>**: Calculate the mean of given numbers\n"
-        "💥 **!troll <count>**: Spam a specified number of messages\n\n"
-        "🐸 **!send_meme <subreddit>**: Fetch a meme from a specified subreddit\n\n"
-        "Use `!command_name` to call each command. Enjoy using the bot! 😄"
-    )
-
-    await ctx.reply(command_list)
 
 
 for filename in os.listdir("./cogs"):
